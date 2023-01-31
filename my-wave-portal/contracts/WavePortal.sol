194087eff58e98a8ab1f1b1386114d5be49b9f18
Kaustubh <kaustubhthakur76@gmail.com>
2023-01-31 18:53:15 +0530

    initial commit

---
+++ my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Wave at us guys");
+    }
+}
